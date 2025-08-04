8f3a3af6f50c22fd1136f83f5c883422d9fc3e1e
Bee Jay <abiolaabeeola159@gmail.com>
2025-08-04 13:19:35 +0100

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,32 @@ import { Orbitron } from 'next/font/google';
 
 const orbitron = Orbitron({
   subsets: ['latin'],
-  weight: '500', // Customize as needed
+  weight: '700', // Customize as needed
   variable: '--font-orbitron',
 });
 
 export default function Footer() {
   return (
     <div className={` py-16 !w-[95vw] w-12 mx-auto`}>
-      <div className="w-1/2">
-        <h1 className={orbitron.className} >ASHUR</h1>
-        <h2 className={`text-gray-500`}>Get Newsletter updates from Ashur</h2>
+      <hr className='mb-12'/>
+      <div className="flex justify-between items-center mb-8">
+        <div className="w-1/3">
+          <h1 className={`${orbitron.className} text-7xl`} >ASHUR</h1>
+          <h2 className={`text-gray-400 text-xs`}>Get Newsletter updates from upcoming product and best discount for them all</h2>
+          <form className="mt-4 grid grid-cols-3 gap-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="col-span-2 px-3 border-2 border-gray-300 focus:outline-none rounded-full"
+            />
+            <button
+              type="submit"
+              className="col-span-1 bg-black text-white py-3 rounded-full hover:bg-gray-800 transition"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   )
